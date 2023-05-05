a57c91e3b665d385d95d42e94548341efa77c64c
Christopher Healy <chris.healy33@gmail.com>
2023-05-05 13:00:22 +0100

    test commit again!

--- src/main/java/com/chris/ca2/CA2.java
+++ src/main/java/com/chris/ca2/CA2.java
@@ -12,6 +12,6 @@ package com.chris.ca2;
 public class CA2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("welcome!");
     }
 }
