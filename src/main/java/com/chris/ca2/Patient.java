bc4329e792204ea81a247c8e3eb8f64c52767ecf
Christopher Healy <chris.healy33@gmail.com>
2023-05-06 23:01:45 +0100

    Patient class initial set up

---
+++ src/main/java/com/chris/ca2/Patient.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chris.ca2;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author chris
+ */
+public class Patient {
+
+    String fName;
+    String sName;
+    LocalDate dob;
+    LocalDate joinDate;
+    //• The appointments associated with the Patient (an Appointment LinkedList). 
+
+//Two Patients are considered equal where they have the same first name, second name and date of birth (irrespective of the rest
+//of their fields). 
+}
